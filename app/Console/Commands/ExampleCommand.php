a8c9f444273f516372cf20d4ce45f7b4a7ed52f4
Michael <michael.bitokhov@gmail.com>
2018-01-18 00:48:37 -0500

    Add Console Commands for testing purposes

---
+++ app/Console/Commands/ExampleCommand.php
@@ -0,0 +1,24 @@
+<?
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ExampleCommand extends Command
+{
+    protected $signature = '';
+
+    protected $description = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+
+    }
+}
+
+
---
+++ app/Console/Commands/Sandbox.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Sandbox extends Command
+{
+    protected $signature = 'sandbox:run';
+
+    protected $description = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+
+    }
+}
+
+
