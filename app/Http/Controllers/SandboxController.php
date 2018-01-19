7b7a541136ebda3747eed7b1bf80f4a86b1db865
Michael <michael.bitokhov@gmail.com>
2018-01-19 00:22:13 -0500

    Add Sandbox Controller

---
+++ app/Http/Controllers/SandboxController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SandboxController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getModel()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function createModel()
+    {
+
+    }
+
+    public function updateModel()
+    {
+
+    }
+
+    public function deleteModel()
+    {
+
+    }
+}
