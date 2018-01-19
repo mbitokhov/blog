6b68d1fd465f25d3d09d16b1b66691f53f02e9f7
Michael <michael.bitokhov@gmail.com>
2018-01-19 00:46:40 -0500

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$controllers = [
+    'BlogPostController' => '/blog_posts',
+];
+
+if(env('APP_ENV') !== 'production')
+{
+    $controllers['SandboxController'] = '/sandbox';
+}
+
+foreach($controllers as $controller => $route)
+{
+    $router->get   ($route          , $controller . '@index');
+    $router->get   ($route . '/{id}', $controller . '@getModel');
+    $router->post  ($route          , $controller . '@createModel');
+    $router->put   ($route . '/{id}', $controller . '@updateModel');
+    $router->delete($route . '/{id}', $controller . '@deleteModel');
+}
