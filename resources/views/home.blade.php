3ca423a1fdb4d7ee533fdcb5fb8fb679ade4e12b
Michael Bitokhov <michael.bitokhov@gmail.com>
2018-06-19 00:30:26 -0400

    Add has-shadow testing to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,16 @@
-@extends('templates.mainbox-with-sidebar')
+@extends('templates.template')
 
-@section('inner')
-    <div>
-        foo
-    </div>
-@endsection
-
-@section('sidebar')
-    <div>
-        baz
-    </div>
+@section('content')
+  <div>
+    @foreach(array('darkest', 'darker', 'dark', 'normal', 'light', 'lighter', 'lightest') as $v)
+      <div class="box has-shadow-{{ $v }}">
+        {{$v}}
+      </div>
+    @endforeach
+    @foreach(array('has-shadow', 'has-no-shadow') as $v)
+      <div class="box {{$v}}">
+        {{$v}}
+      </div>
+    @endforeach
+  </div>
 @endsection
