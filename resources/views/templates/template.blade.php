94d2c3d4ec872f042650abe369c941f942c10bad
Michael Bitokhov <michael.bitokhov@gmail.com>
2018-07-25 00:44:55 -0400

    Use unescaped {!! !!} version

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -14,13 +14,13 @@
             rel="stylesheet"
             type="text/css"
             href="{{ assets('css/bulma.css') }}"
-            integrity="sha512-{{ App\Services\FileHasher::make(public_path('css/bulma.css')) }}"
+            integrity="sha512-{!! App\Services\FileHasher::make(public_path('css/bulma.css')) !!}"
             crossorigin="anonymous" />
         <link
             rel="stylesheet"
             type="text/css"
             href="{{ assets('css/app.css') }}"
-            integrity="sha512-{{ App\Services\FileHasher::make(public_path('css/app.css')) }}"
+            integrity="sha512-{!! App\Services\FileHasher::make(public_path('css/app.css')) !!}"
             crossorigin="anonymous"/>
     </head>
     <body>
@@ -39,17 +39,17 @@
         @include('templates.footer')
         <script
             src="{{ assets('js/manifest.js') }}"
-            integrity="sha512-{{ App\Services\FileHasher::make(public_path('js/manifest.js'), 'sha512') }}"
+            integrity="sha512-{!! App\Services\FileHasher::make(public_path('js/manifest.js'), 'sha512') !!}"
             crossorigin="anonymous"
         ></script>
         <script
             src="{{ assets('js/vendor.js') }}"
-            integrity="sha512-{{ App\Services\FileHasher::make(public_path('js/vendor.js'), 'sha512') }}"
+            integrity="sha512-{!! App\Services\FileHasher::make(public_path('js/vendor.js'), 'sha512') !!}"
             crossorigin="anonymous"
         ></script>
         <script
             src="{{ assets('js/app.js') }}"
-            integrity="sha512-{{ App\Services\FileHasher::make(public_path('js/app.js'), 'sha512') }}"
+            integrity="sha512-{!! App\Services\FileHasher::make(public_path('js/app.js'), 'sha512') !!}"
             crossorigin="anonymous"
         ></script>
     </body>
